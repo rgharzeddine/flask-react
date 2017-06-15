5ea3ee98032e265261ec3a29785c676b5619f311
Rawad Gharzeddine <rawad.gharzeddine@rezayat.net>
2017-06-15 11:49:29 +0300

    part 1 nginx deployed

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import unittest
 from flask_script import Manager
 
 from project import create_app, db
-# from project.api.models import User
+from project.api.models import User
 
 
 app = create_app()
@@ -26,6 +26,20 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+
+@manager.command
+def seed_db():
+    """Seeds the database."""
+    db.session.add(User(username='michael', email="michael@realpython.com"))
+    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.commit()
+
+
+@manager.command
+def reset_db():
+    recreate_db()
+    seed_db()
 
 
 if __name__ == '__main__':
---
+++ notes.md
@@ -0,0 +1,18 @@
+# Notes
+
+## Docker
+
+**Problem**
+
+**docker-compose up -d** did not start
+docker had problems mounting volume
+
+**Solution**
+
+1. add user to docker group
+2. use native machine on linux and do not build a machine
+
+------------------------------------------------------
+
+**Problem**
+**Solution**
